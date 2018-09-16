93317587c441eea4427798b5e6a57d64590465ff
Armaan Shah <armaanshah96@gmail.com>
2018-09-16 15:07:20 -0400

    exercise 5

---
+++ ex5.sql
@@ -0,0 +1,8 @@
+/*query one*/
+SELECT name, age FROM pet WHERE age > 10;
+
+/*query 2 and query 3 criteria fits within this*/
+SELECT first_name, age FROM person WHERE first_name != "Armaan" AND age < 22;
+
+/*query four*/
+SELECT first_name, age, id FROM person WHERE (first_name != "Zed" AND id < 1000) OR age > 1000;
