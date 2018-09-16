11321e2980430ca896859f40391e635b58308227
Armaan Shah <armaanshah96@gmail.com>
2018-09-16 15:17:26 -0400

    exercise 6

--- ex5.sql
+++ ex5.sql
@@ -2,7 +2,7 @@
 SELECT name, age FROM pet WHERE age > 10;
 
 /*query 2 and query 3 criteria fits within this*/
-SELECT first_name, age FROM person WHERE first_name != "Armaan" AND age < 22;
+SELECT id, first_name, age FROM person WHERE first_name != "Armaan" AND id=1 AND age < 22;
 
 /*query four*/
 SELECT first_name, age, id FROM person WHERE (first_name != "Zed" AND id < 1000) OR age > 1000;
---
+++ ex6.sql
@@ -0,0 +1,6 @@
+/*query 1: find pets youve added so far*/
+SELECT * from pet;
+
+
+/*query 2: change queries to use your person.id instead of person.name */
+/* edited previous ex[1-6].sql files to fix */
